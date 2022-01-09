9ee42c5a208103411a40864b03aab3ede41a97be
Doonywoony <javismcarr@gmail.com>
2022-01-08 23:01:12 -0500

    added a few more questions

--- index.js
+++ index.js
@@ -7,19 +7,50 @@ const util = require('util');
 // TODO: Create an array of questions for user input
 const questions = [{
    
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your repository? (Required)',
-      //validate to make sure there is a value there
-  validate: nameInput => {
-    if (nameInput) {
-      return true;
-    } else {
-      console.log('Please enter your repository title.');
-      return false;
-    }
-  }
-}];
+    
+        type: "input",
+        name: "accountName",
+        message: "What is your github username? (Required)",
+        validate: (accountNameInput) => {
+          if(accountNameInput) {
+            return true;
+          } else {
+            console.log("Please enter your GitHub username!");
+            return false;
+          }
+        },
+    },
+      {
+        type: "input",
+        name: "accountEmail",
+        message: "What is your github email address? (Required)",
+        validate: (accountEmailInput) => {
+          if(accountEmailInput) {
+            return true;
+          } else {
+            console.log("Please enter your GitHub email address!");
+            return false;
+          }
+        },
+      },
+
+      {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the name of the project repository? (Required)",
+        validate: (projectTitleInput) => {
+          if(projectTitleInput) {
+            return true;
+          } else {
+            console.log("Please enter your project title!");
+            return false;
+          }
+        },
+      },
+
+     
+    
+        ];
 
 
 // TODO: Create a function to write README file
