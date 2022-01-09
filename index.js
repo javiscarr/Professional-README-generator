6471eda9a2a1ede55b6454bbbdc5a488aae88416
Doonywoony <javismcarr@gmail.com>
2022-01-08 22:03:30 -0500

    a couple of questions added

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+   
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your repository? (Required)',
+      //validate to make sure there is a value there
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter your repository title.');
+      return false;
+    }
+  }
+},
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
