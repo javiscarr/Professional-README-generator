8e07bd37c5cc8529ab68e2685bfcd1eb471f043f
Doonywoony <javismcarr@gmail.com>
2022-01-14 16:18:22 -0500

    added contributions, usage, installation, questions sections/added avaialibity for questions to be asked

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -30,8 +30,97 @@ function generateMarkdown(data) {
     licenseOption = 'TheUnlicense';
     licenseLink = 'https://choosealicense.com/licenses/unlicense/';
   };
+//badges to be added to the README
+  const generateBadges = (badges,user,repo)=> {
+    let badgeArray = [];
+    if(!badges) {
+      return '';
+    }
+    badges.forEach(e => {
+      switch(e){
+        case "Language Count":
+          badgeArray.push('![GitHub language count](https://img.shields.io/github/languages/count/' + user + '/' +repo +')');
+          break;
+        case "Top Language":
+          badgeArray.push('![GitHub top language](https://img.shields.io/github/languages/top/'+user+'/'+repo+')');
+          break;
+        case "Code Size":
+          badgeArray.push('![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/'+user+'/'+repo+')');
+          break;
+        case "Repo Size":
+          badgeArray.push('![GitHub repo size](https://img.shields.io/github/repo-size/'+user+'/'+repo+')');
+          break;
+        case "Issues":
+          badgeArray.push('![GitHub issues](https://img.shields.io/github/issues-raw/'+user+'/'+repo+')');
+          break;
+        case "Issues Closed":
+          badgeArray.push('![GitHub closed issues](https://img.shields.io/github/issues-closed-raw/'+user+'/'+repo+')');
+          break;
+        case "Release Version by Date":
+          badgeArray.push('![GitHub release (latest by date)](https://img.shields.io/github/v/release/'+user+'/'+repo+')');
+          break;
+        default:
+          break;
+      }
+    });
+    return badgeArray.join(' ');
+  };
+// variable is created that will hold markdown template and add to it dynamically 
+let markdownTemplate =
+    
+//title, badge, and description
+    
+`# ${data.projectTitle}
+## Description
+${data.description}
+![badge](https://img.shields.io/badge/license-${licenseOption}-brightorange)
+You can access more badges and their purposes at [shields.io](https://shields.io)
+`;
 
+${generateBadges(data.badges,data.username,data.projectTitle)}
+  
+let tableOfContents =
+`## Table of Contents`;
+  if (data.installation) {
+    tableOfContents +=
+      `
+  * [Installation](#installation)`
+  };
+    if (data.instructions) {
+      tableOfContents +=
+      `
+      * [Usage](#usage)`
+   };
+    if (data.contribution) {
+      tableOfContents +=
+        `
+        * [Installation](#installation)`
+    };
+    if (data.instructions) {
+        tableOfContents +=
+        `
+        * [Installation](#installation)`
+   };
+    if (data.instructions) {
+     tableOfContents +=
+     `
+     * [Contribution](#contribution)`
+  };
+  if (data.testing) {
+    tableOfContents +=
+      `
+  * [Testing](#testing)`
+  };
+  //append table to template
+  markdownTemplate += tableOfContents;
 
-
+  //add contact info and license (required)
+  markdownTemplate +=
+    `
+  * [Questions](#questions)`;
+  markdownTemplate +=
+    `
+  * [License](#license)
+    
 
 module.exports = generateMarkdown;
